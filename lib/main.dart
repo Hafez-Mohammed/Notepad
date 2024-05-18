5698ab323f39cc4994b9b49d0ba838cb912d1070
Hafez Mohammed <hafezmd1282000@gmail.com>
2024-05-18 03:47:57 -0500

    edit title of MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Notepad",
       theme: ThemeData(
           colorScheme: ColorScheme(
               brightness: Brightness.light,
